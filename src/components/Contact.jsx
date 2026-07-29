f1ea856104d0109b79de21acae41de146f9b25b7
Adewunmi Dolapo <adewunmidolapo010@gmail.com>
2026-07-29 09:04:23 -0700

    jodas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,17 +5,19 @@ import gmail from "../assets/gmail.png";
 function Contact() {
 
   return(
-    <div className="contact-info">
+    <section id="contact">
+      <div className="contact-info">
       <h1>Contact Us</h1>
       <p>For More Enquires <i>reach out on:</i></p>
       <div>
-        <a href="https://wa.me/2348101217454?text=Hi,%20can%20I%20know%20more%20about%20your%20service?"><img src={whatsapp} alt="whatsapp" width={50} /></a>
+        <a href="https://wa.me/2348101217454?text=Hi,%20can%20I%20know%20more%20about%20your%20services?"><img src={whatsapp} alt="whatsapp" width={50} /></a>
         <br />        
         <a href="https://www.tiktok.com/@jodas_technologies"><img src={tiktok} alt="tiktok" width={40} /></a>
         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=adewunmioluwadamilare09@gmail.com&su=Hello&body=Can%20I%20know%more%20about%20you!"><img src={gmail} alt="gmail" width={40} /></a>
-        <a href="https://www.facebook.com/adewunmi.oluwadamilare.2025?text=Hi,%20can%20I%20know%20more%20about%20your%20service?"><img src={facebook} alt="facebook" width={40} /></a>
+        <a href="https://www.facebook.com/adewunmi.oluwadamilare.2025?text=Hi,%20can%20I%20know%20more%20about%20your%20services?"><img src={facebook} alt="facebook" width={40} /></a>
       </div>
     </div>
+    </section>
   )
 }
 export default Contact;
